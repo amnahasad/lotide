d2efac952862bc2a9203c5aa004a4527e2496b61
amnahasad <amnah_asad@hotmail.com>
2021-10-26 00:22:34 +0000

    Completed the assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log("😊😊😊 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("hello", "hello");
+assertEqual(1, 1);
+assertEqual(5, 2);
